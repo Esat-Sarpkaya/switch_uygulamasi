9b542b5b2e69410c4db8f10f4c89e051282074c1
Esat Sarpkaya <32740293+envefe@users.noreply.github.com>
2018-09-13 08:39:47 +0300

    Update switchlerPost.php

--- switchlerPost.php
+++ switchlerPost.php
@@ -3,26 +3,42 @@ include 'baglan.php';
 if((strlen($_POST['s_adi']) > 0) && (strlen($_POST['ip_no']) > 0))
 {
     $s_adi = $_POST['s_adi'];
-    //$s_durum = $_POST['s_durum'];
     $ip_no = $_POST['ip_no'];
-
     $sql = "insert into switch (s_adi,ip_no) values ( '".$s_adi."', '".$ip_no."') ";
+//--------------------------------------------------------------------------------------
+    $kontrol= isset($_POST["submit"]) ? $_POST["submit"] : '';
 
-    if(mysqli_query($conn,$sql))
+    $adKontrol = mysqli_query($conn, "SELECT * FROM switch WHERE s_adi='$s_adi'");
+    $ipKontrol = mysqli_query($conn, "SELECT * FROM switch WHERE ip_no='$ip_no'");
+
+    if(mysqli_num_rows($adKontrol) > 0)
     {
-        $sorguswitch = "select * from switch where s_adi='" . $s_adi . "' 
-        and ip_no='" .$ip_no. "'";
-
-        /*echo 'Switch Kaydı Yapıldı';*/
+        echo "bu switch ismi daha once kullanilmistir..";
+        header("Refresh: 1; url=switchler.php");
+    }
+    else if (mysqli_num_rows($ipKontrol) > 0)
+    {
+        echo "bu ip numarasi daha once kullanilmistir..";
         header("Refresh: 1; url=switchler.php");
     }
     else
     {
-        echo 'Hata : ' .mysqli_error($conn);
+        if(mysqli_query($conn,$sql))
+        {
+            $sorguswitch = "select * from switch where s_adi='" . $s_adi . "' and ip_no='" .$ip_no. "'";
+
+            echo 'Switch Kaydı Yapıldı';
+            header("Refresh: 1; url=switchler.php");
+        }
+        else
+        {
+            echo 'Hata : ' .mysqli_error($conn);
+        }
     }
 }
+//---------------------------------------------------------------------------------------
 else
 {
     echo 'Verileri Eksiksiz Girin';
-    header("Refresh: 5; url=switchler.php");
+    header("Refresh: 1; url=switchler.php");
 }
