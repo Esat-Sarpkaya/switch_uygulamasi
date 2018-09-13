26e0af5604fa59bb5c79a1a99a5435a19b6d7a6c
Esat Sarpkaya <32740293+envefe@users.noreply.github.com>
2018-09-13 08:38:51 +0300

    Update baglamaPost.php

--- baglamaPost.php
+++ baglamaPost.php
@@ -10,15 +10,34 @@ if((strlen($_POST['s_adi']) > 0) && (strlen($_POST['port1']) > 0) && (strlen($_P
     $sql = "insert into baglama (s_adi, port1, s_adi2, port2) 
             values ( '" .$s_adi. "', '" .$port1. "', '" .$s_adi2. "', '" .$port2. "')";
 
-    if(mysqli_query($conn,$sql))
+    $kontrol= isset($_POST["submit"]) ? $_POST["submit"] : '';
+    $port1Kontrol = mysqli_query($conn, "SELECT * FROM baglama WHERE port1='$port1'");
+    $port2Kontrol = mysqli_query($conn, "SELECT * FROM baglama WHERE port2='$port2'");
+//-----------------------------------------------------------------------------------------------
+
+    if(mysqli_num_rows($port1Kontrol) > 0)
     {
-        /*echo 'Switchler portlar ile bağlandı.';*/
+        echo "bu port şu anda kullanilmaktadır..";
+        header("Refresh: 1; url=baglama.php");
+    }
+    else if (mysqli_num_rows($port2Kontrol) > 0)
+    {
+        echo "bu port şu anda kullanilmaktadır..";
         header("Refresh: 1; url=baglama.php");
     }
 
+//-----------------------------------------------------------------------------------------------
     else
     {
-        echo 'Hata : ' .mysqli_error($conn);
+        if (mysqli_query($conn, $sql))
+        {
+            echo 'Switchler portlar ile bağlandı.';
+            header("Refresh: 1; url=baglama.php");
+        }
+        else
+        {
+            echo 'Hata : ' . mysqli_error($conn);
+        }
     }
 }
 else
