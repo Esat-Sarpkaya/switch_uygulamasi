85b6763e4054c33276b333883dd4d1dfb9633acc
Esat Sarpkaya <32740293+envefe@users.noreply.github.com>
2018-09-13 08:38:12 +0300

    Update baglama.php

--- baglama.php
+++ baglama.php
@@ -1,20 +1,6 @@
 <html>
 <head>
     <title>Bağlama</title>
-    <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.2.min.js"></script>
-    <script type="text/javascript">
-        $(function()
-        {
-            $.esat =
-                {
-                    gonder: function ()
-                    {
-                        alert("Switchler Portlar ile Bağlandı.");
-                    }
-                }
-        });
-
-    </script>
     <meta charset="UTF-8">
     <link rel="stylesheet" type="text/css" href="duzen.css">
 </head>
@@ -30,7 +16,7 @@
 
             <tr>
                 <td>Switch Seç</td>
-                <td>
+                <td colspan="5">
                     <select class="girisInput" type="text" name="s_adi" id="s_adi">
 
                         <?php
@@ -52,13 +38,13 @@
             </tr>
             <tr>
                 <td>Switch'e Bağlı Port</td>
-                <td><input class="girisInput" type="text" name="port1" id="port1"
+                <td colspan="5"><input class="girisInput" type="text" name="port1" id="port1"
                            placeholder="Port Numarası Girin...">
                 </td>
             </tr>
             <tr>
                 <td>Üst Switch Seç</td>
-                <td>
+                <td colspan="5">
                     <select class="girisInput" type="text" name="s_adi2" id="s_adi">
                         <?php
                         $sorguswitch = "select * from switch";
@@ -79,44 +65,71 @@
             </tr>
             <tr>
                 <td>Üst Switch'e Bağlı Port</td>
-                <td><input class="girisInput" type="text" name="port2" id="port2"
+                <td colspan="5"><input class="girisInput" type="text" name="port2" id="port2"
                            placeholder="Port Numarası Girin...">
                 </td>
             </tr>
             <tr>
-                <td colspan="2">
-                    <input type="submit" value="Bağla" class="girisInput" onclick="$.esat.gonder()">
+                <td height="40" colspan="5">
+                    <input type="submit" value="Bağla" class="girisInput">
                 </td>
             </tr>
 
             <tr>
                 <a href="switchler.php">Switch Oluştur Sayfasına Dön
                 </a>
+            </tr></table>
+
+            <table border="1" width="100%">
+            <tr>
+                <td>Bağlanmış Switchler ve Portlar:</td>
+                <?php
+                include 'baglan.php';
+                ?><td width="150"><?php
+                    $s = "SELECT * FROM baglama";
+                    if ($sorgu = mysqli_query($conn, $s))
+                    {
+                        while ($row = mysqli_fetch_assoc($sorgu))
+                        {
+                            echo $row['s_adi'];
+                            echo('<br>');
+                        }
+                    }?></td>
+
+                <td width="45"><?php
+                    $t = "SELECT * FROM baglama";
+                    if ($sorgu2 = mysqli_query($conn, $t))
+                    {
+                        while ($row = mysqli_fetch_assoc($sorgu2))
+                        {
+                            echo $row['port1'];
+                            echo('<br>');
+                        }
+                    }
+                    ?></td>
+                <td width="150"><?php
+                    $k = "SELECT * FROM baglama";
+                    if ($sorgu = mysqli_query($conn, $k))
+                    {
+                        while ($row = mysqli_fetch_assoc($sorgu))
+                        {
+                        echo $row['s_adi2'];
+                        echo('<br>');
+                        }
+                    }?></td><td width="45"><?php
+
+                    $l = "SELECT * FROM baglama";
+                    if ($sorgu2 = mysqli_query($conn, $l))
+                    {
+                        while ($row = mysqli_fetch_assoc($sorgu2))
+                        {
+                            echo $row['port2'];
+                            echo('<br>');
+                        }
+                    }?></td>
             </tr>
 
         </table>
-        <tr>
-            <td>Bağlanmış Switchler ve Portlar:</td>
-            <td><?php
-                include 'baglan.php';
-                $s = "select * from baglama";
-                if ($sorgu = mysqli_query($conn, $s))
-                {
-                    while ($row = mysqli_fetch_assoc($sorgu))
-                    {
-                        echo $row['s_adi'];
-                        echo " ";
-                        echo $row['port1'];
-                        echo " ";
-                        echo $row['s_adi2'];
-                        echo " ";
-                        echo $row['port2'];
-                        echo "; ";
-                    }
-                }
-                ?>
-            </td>
-        </tr>
     </form>
 </div>
 </body>
